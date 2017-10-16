9b8de8067d7e542db408ddc9f6b562ce18560fb8
Unknown <spruur@gmail.com>
2017-10-16 16:33:14 +0300

    Added different returning options

--- src/Dec4Math/Decimal4.php
+++ src/Dec4Math/Decimal4.php
@@ -11,22 +11,125 @@ namespace Dec4Math;
 class Decimal4 {
     const SEPARATOR = '.';
 
-    private $mill;
+    /**
+     * @var string
+     */
+    private $mills;
 
+    /**
+     * @param string $string Fraction part must be separated from integer part with
+     * the dot (.) symbol.
+     */
     public function __construct($string)
     {
-
         if (strpos($string, self::SEPARATOR) === false) {
-            $this->mill = gmp_strval(
+            $this->mills = gmp_strval(
                 gmp_mul(
                     gmp_init($string,10),
-                100
+                10000
                 )
             );
-            echo $this->mill;
             return;
+        }
+
+        $parts = explode(self::SEPARATOR, $string);
+
+        while (strlen(trim($parts[1])) < 4) {
+            $parts[1] .= '0';
+        }
+
+        $this->mills = gmp_strval(
+            gmp_add(
+                gmp_mul(
+                    gmp_abs(gmp_init($parts[0], 10)),
+                    10000
+                ),
+                self::roundMills(trim($parts[1]))
+            )
+        );
+
+        if (trim($string[0]) === '-') {
+            $this->mills = gmp_strval(gmp_neg(gmp_init($this->mills)));
         }
     }
 
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        $zeros = str_repeat('0', 4 - strlen(abs($this->fractionValue())));
+        return $this->integerValue().self::SEPARATOR.$zeros.(abs($this->fractionValue()));
+    }
 
+    /**
+     * The integer part of this amount (without mills).
+     *
+     * @return integer Or string, if the number is too big.
+     */
+    public function integerValue() {
+        $sign = (
+            gmp_cmp(
+                gmp_init($this->mills, 10),
+                0
+            ) < 0
+        ) ? '-' : '';
+
+        return $sign.gmp_strval(
+            gmp_div_q(
+                gmp_abs(
+                    gmp_init($this->mills, 10)
+                ),
+                10000
+            ),
+            10
+        );
+    }
+
+
+    /**
+     * Returns the amount of mills in this decimal fraction part.
+     *
+     * @return integer
+     */
+    public function fractionValue() {
+        $return = gmp_div_r(
+            gmp_abs(
+                gmp_init($this->mills, 10)
+            ),
+            10000
+        );
+
+        if (gmp_cmp(gmp_init($this->mills, 10), 0) < 0)
+            $return = gmp_neg($return);
+
+        return gmp_intval($return);
+    }
+
+
+    /**
+     * Returns entire amount in mills.
+     *
+     * @return integer Or string, if the number is too big.
+     */
+    public function millsValue() {
+        if (gmp_cmp(gmp_abs(gmp_init($this->mills, 10)), PHP_INT_MAX) > 0) {
+            return $this->mills;
+        }
+        return gmp_intval(gmp_init($this->mills, 10));
+    }
+
+    private static function roundMills($num) {
+        $digitsAmount = strlen($num);
+
+        if ($digitsAmount < 5) return gmp_init($num, 10);
+
+        $divider = gmp_pow(10, $digitsAmount - 4);
+        $addition = gmp_mul(5, gmp_pow(10, $digitsAmount - 5));
+        return gmp_div_q(
+            gmp_add(gmp_init($num, 10), $addition),
+            $divider,
+            GMP_ROUND_ZERO);
+    }
 }
